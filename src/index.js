6c5ef443079ed6f99e5db1bad4d778632cf2c29a
Maxim Novikov <maximkanv@gmail.com>
2021-02-18 08:43:46 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 
-console.log('asd!');
+function runApp() {
+  const url = 'https://api.spotify.com/v1/artists/ID';
+  return axios.get(url).then((response) => console.log(response));
+}
+runApp();
