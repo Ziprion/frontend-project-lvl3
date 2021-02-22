063a18ca081b5b2feffe25d0cb739ff9e8a2063c
Maxim Novikov <maximkanv@gmail.com>
2021-02-22 13:59:50 +0300

    asd

--- src/init.js
+++ src/init.js
@@ -9,28 +9,28 @@ export default () => {
   i18n.init({
     lng: 'en',
     resources,
-  }).then(() => {
-    const state = {
-      process: 'filling',
-      errors: '',
-      feeds: [],
-      posts: [],
-      uniq: 0,
-    };
-    const watchedState = view(state);
-    const form = document.querySelector('form');
-    const submit = document.querySelector('button[aria-label="add"]');
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      watchedState.process = 'checking';
-      state.errors = '';
-      const formData = new FormData(e.target);
-      const url = formData.get('url');
-      checkURL(watchedState, url);
-      form.reset();
-      form.focus();
-      submit.blur();
-    });
-    setTimeout(checkRSS, 5000, state);
+  }, function(err, t) {const state = {
+    process: 'filling',
+    errors: '',
+    feeds: [],
+    posts: [],
+    uniq: 0,
+  };
+  const watchedState = view(state);
+  const form = document.querySelector('form');
+  const submit = document.querySelector('button[aria-label="add"]');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    watchedState.process = 'checking';
+    state.errors = '';
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    checkURL(watchedState, url);
+    form.reset();
+    form.focus();
+    submit.blur();
   });
+  setTimeout(checkRSS, 5000, state);})
+    
+  
 };
