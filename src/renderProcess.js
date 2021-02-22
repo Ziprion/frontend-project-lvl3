7e656d9038c30662177de646afa014a13c53ecd2
Maxim Novikov <maximkanv@gmail.com>
2021-02-22 19:33:10 +0300

    asd

--- src/renderProcess.js
+++ src/renderProcess.js
@@ -1,15 +1,15 @@
 export default (state) => {
   console.log(state);
-  const input = document.querySelector('input');
+  // const input = document.querySelector('input');
   const submit = document.querySelector('button[aria-label="add"]');
   if (state.process === 'checking') {
     const feedback = document.querySelector('.feedback');
     feedback.classList.remove('text-success', 'text-danger');
     feedback.innerHTML = '';
     submit.disabled = true;
-    input.setAttribute('readonly', '');
+    // input.setAttribute('readonly', '');
   } else {
     submit.disabled = false;
-    input.removeAttribute('readonly');
+    // input.removeAttribute('readonly');
   }
 };
