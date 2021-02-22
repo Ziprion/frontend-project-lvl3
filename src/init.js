8dc9b6cf7e857a11a8a49294e44f9af58b78f103
Maxim Novikov <maximkanv@gmail.com>
2021-02-22 14:09:28 +0300

    asd

--- src/init.js
+++ src/init.js
@@ -1,36 +1,36 @@
 import 'bootstrap';
-import i18n from 'i18next';
+import i18next from 'i18next';
 import checkURL from './checkURL.js';
 import view from './view.js';
 import resources from './locales';
 import checkRSS from './checkRSS.js';
 
 export default () => {
-  i18n.init({
-    lng: 'en',
+  i18next.init({
+    lng: 'ru',
     resources,
-  }, function(err, t) {const state = {
-    process: 'filling',
-    errors: '',
-    feeds: [],
-    posts: [],
-    uniq: 0,
-  };
-  const watchedState = view(state);
-  const form = document.querySelector('form');
-  const submit = document.querySelector('button[aria-label="add"]');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    watchedState.process = 'checking';
-    state.errors = '';
-    const formData = new FormData(e.target);
-    const url = formData.get('url');
-    checkURL(watchedState, url);
-    form.reset();
-    form.focus();
-    submit.blur();
+  }).then(() => {
+    const state = {
+      process: 'filling',
+      errors: '',
+      feeds: [],
+      posts: [],
+      uniq: 0,
+    };
+    const watchedState = view(state);
+    const form = document.querySelector('form');
+    const submit = document.querySelector('button[aria-label="add"]');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      watchedState.process = 'checking';
+      state.errors = '';
+      const formData = new FormData(e.target);
+      const url = formData.get('url');
+      checkURL(watchedState, url);
+      form.reset();
+      form.focus();
+      submit.blur();
+    });
+    setTimeout(checkRSS, 5000, state);
   });
-  setTimeout(checkRSS, 5000, state);})
-    
-  
 };
--- src/renderRSS.js
+++ src/renderRSS.js
@@ -1,11 +1,11 @@
-import i18n from 'i18next';
+import i18next from 'i18next';
 
 export default (state) => {
   const feedback = document.querySelector('.feedback');
   feedback.classList.remove('text-success', 'text-danger');
   if (state.errors === 'ok') {
     feedback.classList.add('text-success');
-    feedback.textContent = i18n.t('success');
+    feedback.textContent = i18next.t('success');
     return;
   }
   feedback.classList.add('text-danger');
@@ -13,13 +13,13 @@ export default (state) => {
   let message = '';
   switch (log) {
     case 'url must be a valid URL':
-      message = i18n.t('noValidUrl');
+      message = i18next.t('noValidUrl');
       break;
     case 'same RSS':
-      message = i18n.t('sameRSS');
+      message = i18next.t('sameRSS');
       break;
     default:
-      message = i18n.t('noRSS');
+      message = i18next.t('noRSS');
       break;
   }
   feedback.textContent = message;
